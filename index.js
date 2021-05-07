993e70831bff3b77c0c34c720ebf09040d7873eb
Catherine Li <catherine.uk.20@gmail.com>
2021-05-07 21:24:18 +0100

    daily exercise done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let h3El = document.createElement("h3")
 h3El.innerText = "Konami Code Sequence"
 
 let paraEl = document.createElement("p")
-paraEl.innerText = "Up, Up, Down, Down, Left, Right, Left, Right, B, A"
+paraEl.innerText = "Up > Up >Down > Down > Left > Right > Left > Right > B > A"
 
 sequenceSection.append(h3El, paraEl)
 logoImg.after(sequenceSection)
@@ -18,11 +18,40 @@ logoImg.after(sequenceSection)
 
 // - Listen for keyboard inputs, and keep track if the user is pressing the keys sequence in the right order. The code should reset if you make a mistake
 
+function trackUserInput(event){
+   
+    key = event.key
+    inputSequence.push(key)   
+
+    if (KonamiSequence[numOfRightInput] === inputSequence[numOfRightInput]){
+        numOfRightInput++
+        
+        if(numOfRightInput === KonamiSequence.length) spinLogo();
+    }
+    else{
+        numOfRightInput = 0
+        inputSequence.length = 0
+    }
+
+    console.log("inputSequence",inputSequence)
+    console.log("numOfRightInput",numOfRightInput)
+
+}
 
 
 // - If the User input the right sequence, make the logo spin
+function spinLogo(){
+    let logo = document.querySelector(".konami-logo")
+    logo.setAttribute( "style", "transform: rotate(360deg)")
+}
 
 
 
 // Run the code
+let numOfRightInput = 0
+const inputSequence = []
+const KonamiSequence = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"]
+
+
 addSequenceDisplay()
+document.body.addEventListener("keydown", trackUserInput)
